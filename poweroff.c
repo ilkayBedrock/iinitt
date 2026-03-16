66f4bf678d35ea595e1edda50981ee6df4ccab69
ilkayBedrock <ilkayxda@aol.com>
2026-03-16 20:21:33 +0300

    Update poweroff.c
    root check

--- poweroff.c
+++ poweroff.c
@@ -3,6 +3,11 @@
 #include <unistd.h>
 
 int main() {
+    if (geteuid() != 0) {
+        fprintf(stderr, "iinitt: please run this program as root\n");
+        return 1;
+    }
+    
     kill(1, SIGTERM);
     return 0;
 }
