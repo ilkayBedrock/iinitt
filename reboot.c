6a33f83eeee1e08ed1842ed8895f42fcbbb58c61
ilkayBedrock <ilkayxda@aol.com>
2026-03-16 20:20:42 +0300

    Update reboot.c
    root check

--- reboot.c
+++ reboot.c
@@ -1,8 +1,14 @@
 // Ilkay Alakay <ilkayxda@aol.com>. Published under GNU GPLv3(.0)
+#include <stdio.h>
 #include <signal.h>
 #include <unistd.h>
 
 int main() {
+    if (geteuid() != 0) {
+        fprintf(stderr, "iinitt: please run this program as root\n");
+        return 1;
+    }
+    
     kill(1, SIGINT);
     return 0;
 }
